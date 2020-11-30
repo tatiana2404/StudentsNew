1a993061f3b3e1d726ad625ceffbc57fb6a8ad76
tatiana2404 <74406991+tatiana2404@users.noreply.github.com>
2020-11-30 17:33:21 +0200

    Add files via upload

--- src/com/company/Group.java
+++ src/com/company/Group.java
@@ -1,75 +1,79 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class Group {
+public class Group implements Voenkom{
     private Student group[] =new Student[10];
+
+    public void setGroup(Student[] group) {
+        this.group = group;
+    }
+
+    public Student[] getGroup() {
+        return group;
+    }
+
     private int k=0;
     public Group(){}
+
     public void addStudent (Student student) throws MyNewException{
 
-        for(int i=0; i<k; i++){
+/*        for(int i=0; i<=k; i++){
             if (student.getRecordBook() == group[i].getRecordBook()) {
                 System.out.println("Этот студент уже есть в группе");
                 break;
             }
-        }
+        }*/
+
         for (int i = 0; i < group.length; i++) {
             if (group[i] == null) {
-                group[i] = student;
-                System.out.println("Студент успешно добавлен в группу. В группе "+k+" студ.");
+                group[i] = new Student(AddFact.newSurname(), AddFact.newName(), AddFact.newAge(), AddFact.newGender(),
+                        AddFact.newGroup(),  AddFact.newCourse(), AddFact.newRecordBook());
+
                 k++;
+                System.out.println("Студент успешно добавлен в группу. В группе "+k+" студ.");
                 return;
             }
 
         }
+
         throw new MyNewException();
     }
-    public void deleteStudent(Student student){
-        int i=1;
-        for(; i<k; i++){
-            if(student.getRecordBook() == group[i].getRecordBook()){
-                group[i]=null;
-                k--;
-                System.out.println("Студен удалён из этой группы. В группе "+k+" студ.");
-                for(; i<k; i++){
-                    group[i]=group[i+1];
-                }
-            }
-        }
+
+    public Student[] sortSurname(){
+        Arrays.sort(group, Comparator.nullsFirst(new SurnameComparator()));
+            return group;
     }
-    public Student findStudent(String surname){
-        for(int i=0; i<k;i++){
-            if(surname.equals(group[i].getSurname()))
-            {
-                System.out.println("Студент найден.");
-                return group[i];
-            }
-        }
-        return null;
+    public Student[] sortSurnameGender(){
+        Arrays.sort(group, Comparator.nullsFirst(new GenderComparator()));
+        return group;
     }
-    public Student[] sortirovka(Student[] grooup){
-        for(int i=0; i<k;i++){
-            String minSurname=grooup[i].getSurname();
-            Student min=grooup[i];
-            int position=i;
-            for (int j=i+1; j<k;j++){
-                if(grooup[j].getSurname().compareTo(minSurname)<0){
-                    position=j;
-                    minSurname=grooup[j].getSurname();
-                    min=grooup[j];
-                }
+
+    @Override
+    public Student[] otbor() {
+        Student[] voen=new Student[k];
+        int n=0;
+        for(int i=0; i<k; i++){
+            if(group[i].getGender().equals(Sex.man) && group[i].getAge()>18 ){
+                voen[0]=group[i];
+                n++;
             }
-            grooup[position]=grooup[i];
-            grooup[i]=min;
         }
-        return grooup;
+        if (n>0){
+            return voen;
+        }
+        else {
+            System.out.println("Таких студентов нет");
+            return null;
+        }
     }
 
     @Override
     public String toString() {
         return "Group{" +
-                "group=" +  Arrays.toString(sortirovka(group)) +
+                "group=" + Arrays.toString(group) +
                 '}';
     }
+
 }
