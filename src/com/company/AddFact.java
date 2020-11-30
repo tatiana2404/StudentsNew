558b1a558846ea0683b72d7e155cf93260fd8c22
tatiana2404 <74406991+tatiana2404@users.noreply.github.com>
2020-11-30 17:34:30 +0200

    Add files via upload

---
+++ src/com/company/AddFact.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class AddFact {
+    public static Scanner str=new Scanner(System.in);
+
+    public static String newSurname(){
+        str.nextLine();
+        System.out.println("Введите фамилию: ");
+        String line = str.nextLine();
+        return line;
+    }
+    public static String newName(){
+        System.out.println("Введите имя: ");
+        String line = str.nextLine();
+        return line;
+    }
+    public static int newAge(){
+        System.out.println("Введите возраст: ");
+         int line = str.nextInt();
+         return line;
+    }
+    public static Sex newGender(){
+        System.out.println("Введите пол: ");
+        str.nextLine();
+        Sex line=Sex.valueOf(str.nextLine());
+        return line;
+
+    }
+    public static String newGroup(){
+        System.out.println("Введите группу: ");
+        if (str.hasNextLine()) {
+            String line = str.nextLine();
+            return line;
+        } else {
+            return "Извините, но это явно не строка. Перезапустите программу и попробуйте снова!";
+        }
+    }
+
+    public static int newCourse(){
+        System.out.println("Введите номер курса: ");
+        if (str.hasNextLine()) {
+            int line = str.nextInt();
+            return line;
+        } else {
+            return 0;
+        }
+    }
+    public static String newRecordBook(){
+        System.out.println("Введите номер зачетной книжки: ");
+        str.nextLine();
+        String line = str.nextLine();
+        return line;
+    }
+
+}
